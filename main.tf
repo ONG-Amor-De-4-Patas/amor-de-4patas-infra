0b677188f47307b210e79ebb8e6e4b60d572b7ec
Nico Pierson <nicogpt@gmail.com>
2021-12-17 12:50:24 -0800

    Add cloudfront to store static content

--- main.tf
+++ main.tf
@@ -19,18 +19,6 @@ resource "aws_elastic_beanstalk_application" "amor_de_4patas_beanstalk" {
   description = "Amor de 4 patas production application"
 }
 
-resource "aws_elb" "main" {
-  name               = "amor-de-4patas-elb"
-  availability_zones = ["us-west-1"]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-}
-
 resource "aws_elastic_beanstalk_environment" "amor_de_4patas_beanstalk_environment" {
   name          = "amor-de-4patas-prod"
   application   = aws_elastic_beanstalk_application.amor_de_4patas_beanstalk.name
@@ -42,13 +30,19 @@ resource "aws_elastic_beanstalk_environment" "amor_de_4patas_beanstalk_environme
     value     = "aws-elasticbeanstalk-ec2-role"
   }
 
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "NODE_ENV"
+    value     = "production"
+  }  
+
 }
 
 data "aws_elastic_beanstalk_hosted_zone" "current" {}
 
 # resource "aws_route53_record" "www" {
-#   zone_id = aws_route53_zone.primary.zone_id
-#   name    = "example.com"
+#   zone_id = data.aws_elastic_beanstalk_hosted_zone.current.id
+#   name    = "amorde4patas.org"
 #   type    = "A"
 
 #   alias {
@@ -57,3 +51,141 @@ data "aws_elastic_beanstalk_hosted_zone" "current" {}
 #     evaluate_target_health = true
 #   }
 # }
+
+resource "aws_cloudfront_origin_access_identity" "static_content_origin" {
+  comment = "Origin for static content"
+}
+
+resource "aws_s3_bucket" "amor_de_4_patas_static_content" {
+  bucket = "amor-de-4-patas-static-content"
+  acl    = "private"
+
+  tags = {
+    Name = "amor-de-4-patas-static-content"
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_cloudfront" {
+  bucket = aws_s3_bucket.amor_de_4_patas_static_content.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "${aws_cloudfront_origin_access_identity.static_content_origin.iam_arn}"
+      },
+      "Action": [
+        "s3:GetObject",
+        "s3:ListBucket"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.amor_de_4_patas_static_content.arn}",
+        "${aws_s3_bucket.amor_de_4_patas_static_content.arn}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.amor_de_4_patas_static_content.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.static_content_origin.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/content/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "blacklist"
+      locations        = ["CN"]
+    }
+  }
+
+  tags = {
+    Environment = "production"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
